940f9c3ae2fb792a4d28c94488ddb42c7b86f788
Wijdan Cederlid <hardrock_17@hotmail.com>
2025-09-04 15:21:58 +0200

    first commit

---
+++ app.py
@@ -0,0 +1,25 @@
+from fastapi import FastAPI
+import os
+
+app = FastAPI()
+
+
+@app.get("/")
+def read_root():
+    return {
+        "method": "GitHub Actions",
+        "message": "Hello from Cloud Run via GitHub Actions!",
+        "framework": "FastAPI",
+    }
+
+
+@app.get("/healthz")
+def read_healthz():
+    return {"status": "ok"}
+
+
+if __name__ == "__main__":
+    port = int(os.getenv("PORT", 8080))
+    import uvicorn
+
+    uvicorn.run("app:app", host="0.0.0.0", port=port, reload=True)
